04b2cd3a693daac1703472b35edad4cad7c63c64
EzeQuezada <20231021.itla.edu.do>
2024-12-28 17:11:34 -0400

    ruta arreglada

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,32 @@
+import { act } from "react";
+
+
+export const todoReducer = (initialState = [], action) => {
+  
+
+    switch (action.type) {
+        case "[TODO] add Todo":
+            return [...initialState, action.payload];
+
+        case "[TODO] Remove Todo":
+        return initialState.filter(todo => todo.id !== action.payload);
+            
+        case "[TODO] Toggle Todo":
+        return initialState.map(todo => {
+            if (todo.id===action.payload) {
+                return  {
+                    ...todo,
+                    donde: !todo.done
+                    
+                }
+                
+            }
+            return todo;
+        });
+            
+    
+        default:
+            return initialState;
+    }
+
+}
